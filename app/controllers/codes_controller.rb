8f9a7e62d6436d562f3134fb723f76580a302540
Bryce Simonds <bsimonds14@gmail.com>
2022-12-20 12:42:19 -0700

    Code new page shows form

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -4,10 +4,15 @@ class CodesController < ApplicationController
   end
 
   def new
-    
+    @code = Code.new
   end
 
   def show
 
+  end
+
+  private
+  def code_params
+    params.require(:code).permit(:title, :difficulty, :problem_description)
   end
 end
--- spec/features/codes/create_spec.rb
+++ spec/features/codes/create_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe 'code challenges creation' do
   describe 'the code challenge new form' do
     it 'renders the new form' do
       visit new_code_path
-
+      save_and_open_page
       expect(page).to have_content('New Code Challenge')
       expect(find('form')).to have_content('Title')
       expect(find('form')).to have_content('Difficulty')
-      expect(find('form')).to have_content('Problem Description')
+      expect(find('form')).to have_content('Problem description')
     end
   end
 end
