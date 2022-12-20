9312da4131bdedeca3201fe7d5d451437ed0492b
Bryce Simonds <bsimonds14@gmail.com>
2022-12-20 12:31:39 -0700

    Code index page lists links to code challenges

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :codes, only: [:index, :show]
+  root to: "codes#index"
+  
+  resources :codes
 end
--- spec/features/codes/index_spec.rb
+++ spec/features/codes/index_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe 'code challenges index' do 
   it 'lists all the code challenges' do 
     code_1 = Code.create(title: 'String Splitter', rating: 5, problem_description: 'The method should return an array of these string pairs such as: abc => ["ab", "c_"]')
-    code_2 = Code.create(title: 'NOT Splitter', rating: 10, problem_description: 'This should not appear')
+    code_2 = Code.create(title: 'Some other challenge', rating: 10, problem_description: 'This should also appear')
 
     visit "/codes"
-    save_and_open_page
+    
     expect(page).to have_link(code_1.title)
-
-    expect(page).to_not have_link(code_2.title)
+    expect(page).to have_link(code_2.title)
   end
 end
