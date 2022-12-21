50ab648b625676a17c1b414cd25476dfcbd0a5bf
Bryce Simonds <bsimonds14@gmail.com>
2022-12-21 14:11:40 -0700

    Test: code show page loads properly

---
+++ spec/features/codes/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'code challenge show page' do 
+  it 'shows the codes title, rating and description' do 
+    code_1 = Code.create(title: 'String Splitter', rating: 5, problem_description: 'The method should return an array of these string pairs such as: abc => ["ab", "c_"]')
+
+    visit code_path(code_1)
+    # save_and_open_page
+    expect(page).to have_content(code_1.title)
+    expect(page).to have_content(code_1.rating)
+    expect(page).to have_content(code_1.problem_description)
+  end
+end
