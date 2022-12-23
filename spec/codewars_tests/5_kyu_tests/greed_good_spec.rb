15251867d0f43760eac2e8c11d74edfe20930ef1
Bryce Simonds <bsimonds14@gmail.com>
2022-12-23 08:04:47 -0700

    Tests: reformatted tests from codewars.com into vscode

--- spec/codewars_tests/5_kyu_tests/greed_good_spec.rb
+++ spec/codewars_tests/5_kyu_tests/greed_good_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 require './codewars/5_kyu/greed_good.rb'
 
-RSpec.describe 'the challenge' do 
-  it 'solves the problem' do 
+RSpec.describe 'the score function' do 
+  it 'should value this as worthless' do 
+    dice = [2, 3, 4, 6, 2]
+    expect(score(dice)).to eq("Should be 0")
+  end
 
+  it 'should value this triplet correctly' do 
+    dice = [2, 2, 2, 3, 3] 
+    expect(score(dice)).to eq("Should be 200")
+  end
+
+  it 'should value this mixed set correctly' do 
+    dice = [2, 4, 4, 5, 4]
+    expect(score(dice)).to eq("Should be 450")
   end
 end
